df7fc8d2147a60e69888ed1692e54647a7f78959
Пожилой Жмых <vladimir.berman@skoltech.ru>
2024-03-13 19:06:49 +0300

    half optimize

--- src/tob/service/TOB.java
+++ src/tob/service/TOB.java
@@ -3,83 +3,103 @@ package tob.service;
 import tob.models.Order;
 import tob.models.PriceUnit;
 
-import java.util.Comparator;
-import java.util.TreeMap;
+import java.util.*;
 
-import static tob.Constants.*;
+import static tob.Constants.MAX_PRICE;
+import static tob.Constants.MIN_PRICE;
 import static tob.enums.Action.SET;
 import static tob.enums.Side.BUY;
 import static tob.enums.Side.SELL;
 
 public class TOB {
-    private final TreeMap<String, Order> orders = new TreeMap<>();
+    private final TreeMap<Character, TreeMap<Integer, TreeMap<Long, TreeMap<String, Order>>>> orders = new TreeMap<>();
 
-    private final TreeMap<String, PriceUnit> priceUnits = new TreeMap<>();
-
-    private final TreeMap<String, String> bestPrices = new TreeMap<>();
 
     public String getBestPrice(String input){
         Order order = new Order(input);
         String response = "-";
         if (SET.equals(order.getAction())) {
-            orders.put(order.getKey(), order);
-            if (!priceUnits.containsKey(order.getPriceKey())){
-                PriceUnit priceUnit = new PriceUnit(order);
-                priceUnits.put(priceUnit.getKey(), priceUnit);
-                if (!bestPrices.containsKey(order.getBestPriceKey())){
-                    bestPrices.put(order.getBestPriceKey(), priceUnit.getKey());
-                    response = priceUnit.toString();
-                }
-                else {
-                    Long bestPrice = priceUnits.get(bestPrices.get(order.getBestPriceKey())).getPrice();
-                    if (priceUnit.getSide().equals(BUY.getSide()) && priceUnit.getPrice() > bestPrice
-                            || priceUnit.getSide().equals(SELL.getSide()) && priceUnit.getPrice() < bestPrice) {
-                        bestPrices.put(order.getBestPriceKey(), priceUnit.getKey());
-                        response = priceUnit.toString();
-                    }
-                }
-            }
-            else {
-                PriceUnit priceUnit = priceUnits.get(order.getPriceKey());
-                priceUnit.addOrder(order);
-                response = priceUnit.toString();
-            }
+            response = handleSetOrder(order);
         }
         else {
-            Order currentOrder = orders.get(order.getKey());
-            currentOrder.setAmountRest(currentOrder.getAmountRest() - order.getAmount());
-            currentOrder.setAction(order.getAction());
-            if (priceUnits.get(currentOrder.getPriceKey()).getKey().equals(bestPrices.get(currentOrder.getBestPriceKey()))) {
-                if (priceUnits.get(currentOrder.getPriceKey()).getAmount() == 0) {
-                    PriceUnit newBestPriceUnit;
-                    if (BUY.equals(currentOrder.getSide())) {
-                        newBestPriceUnit = priceUnits.values().stream().filter(t -> t.getInstrumentId().equals(order.getInstrumentId())).filter(t -> t.getAmount() > 0).max(Comparator.comparing(PriceUnit::getPrice)).orElse(null);
-                    } else {
-                        newBestPriceUnit = priceUnits.values().stream().filter(t -> t.getInstrumentId().equals(order.getInstrumentId())).filter(t -> t.getAmount() > 0).min(Comparator.comparing(PriceUnit::getPrice)).orElse(null);
-                    }
-                    if (newBestPriceUnit != null) {
-                        bestPrices.put(currentOrder.getBestPriceKey(), newBestPriceUnit.getKey());
-                    } else {
-                        PriceUnit limitPriceUnit = priceUnits.get(String.join(";", currentOrder.getBestPriceKey(), getLimitPrice(currentOrder).toString()));
-                        if (limitPriceUnit == null) {
-                            limitPriceUnit = new PriceUnit(currentOrder, getLimitPrice(currentOrder));
-                        }
-                        priceUnits.put(limitPriceUnit.getKey(), limitPriceUnit);
-                        bestPrices.put(currentOrder.getBestPriceKey(), limitPriceUnit.getKey());
-                    }
-                }
-                response = priceUnits.get(bestPrices.get(currentOrder.getBestPriceKey())).toString();
-            }
+            response = handleNotSetOrder(order);
         }
         return response;
     }
 
-    private static Long getLimitPrice(Order order){
-        if (BUY.equals(order.getSide())){
+    private String handleSetOrder(Order order){
+        Character side = order.getSide();
+        Integer instrumentId = order.getInstrumentId();
+        Long price = order.getPrice();
+        TreeMap<Integer, TreeMap<Long, TreeMap<String, Order>>> sideMap = orders.get(side);
+        TreeMap<Long, TreeMap<String, Order>> priceMap = sideMap.get(instrumentId);
+        if (priceMap == null){
+            priceMap = new TreeMap<>();
+            priceMap.put(price, new TreeMap<>(Collections.singletonMap(order.getKey(), order)));
+            sideMap.put(instrumentId, priceMap);
+        }
+        else{
+            TreeMap<String, Order> currentOrders = priceMap.computeIfAbsent(price, k -> new TreeMap<>());
+            currentOrders.put(order.getKey(), order);
+        }
+
+        return getBestPriceBySide(side, instrumentId, priceMap, price);
+    }
+
+    private String handleNotSetOrder(Order order){
+        Character side = order.getSide();
+        Integer instrumentId = order.getInstrumentId();
+        Long price = order.getPrice();
+        TreeMap<Integer, TreeMap<Long, TreeMap<String, Order>>> sideMap = orders.get(side);
+        TreeMap<Long, TreeMap<String, Order>> priceMap = sideMap.get(instrumentId);
+        TreeMap<String, Order> currentOrders = priceMap.get(price);
+        Order currentOrder = currentOrders.get(order.getKey());
+        currentOrder.setAmountRest(currentOrder.getAmountRest() - order.getAmount());
+        return getBestPriceBySide(side, instrumentId, priceMap, price);
+    }
+
+    public TOB(){
+        initialisation();
+    }
+
+    private void initialisation(){
+        orders.put(BUY.getSide(), new TreeMap<>());
+        orders.put(SELL.getSide(), new TreeMap<>());
+    }
+    private static Long getLimitPrice(Character side){
+        if (BUY.equals(side)){
             return MIN_PRICE;
         }
         else{
             return MAX_PRICE;
         }
+    }
+
+    private String getBestPriceBySide(Character side, Integer instrumentId, TreeMap<Long, TreeMap<String, Order>> priceMap, Long price){
+        Long bestPrice;
+        if (BUY.equals(side)) {
+            bestPrice = priceMap.lastKey();
+            if (price < bestPrice){
+                return "-";
+            }
+        }
+        else{
+            bestPrice = priceMap.firstKey();
+            if (price > bestPrice){
+                return "-";
+            }
+        }
+        int amount = priceMap.get(price).values().stream().mapToInt(Order::getAmountRest).sum();
+        if (amount == 0) {
+            priceMap.remove(price);
+            if (priceMap.isEmpty()){
+                bestPrice = getLimitPrice(side);
+            }
+            else {
+                bestPrice = BUY.equals(side) ? priceMap.lastKey() : priceMap.firstKey();
+                amount = priceMap.get(bestPrice).values().stream().mapToInt(Order::getAmountRest).sum();
+            }
+        }
+        return String.join(";", instrumentId.toString(), side.toString(), bestPrice.toString(), Integer.toString(amount));
     }
 }
